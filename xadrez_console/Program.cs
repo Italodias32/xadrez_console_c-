651c7020d0224801f950260d54d457a3e05a8989
Italo Dias <italo_dias.j@hotmail.com>
2020-05-12 21:07:57 -0300

    Diferenciação entre as peças brancas e pretas

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -13,9 +13,9 @@ namespace xadrez_console
                 Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
                 tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.ColocarPeca(new Cavalo(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Cavalo(tabuleiro, Cor.Branca), new Posicao(1, 3));
                 tabuleiro.ColocarPeca(new Dama(tabuleiro, Cor.Preta), new Posicao(2, 4));
-                tabuleiro.ColocarPeca(new Cavalo(tabuleiro, Cor.Preta), new Posicao(0, 7));
+                tabuleiro.ColocarPeca(new Cavalo(tabuleiro, Cor.Branca), new Posicao(0, 7));
 
                PosicaoXadrez posicaoXadrez = new PosicaoXadrez('a', 1);
 
